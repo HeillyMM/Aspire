2b156ba0b48ac87cecfc05a93a43f840a0195f7d
HeillyMM <helenkeylli@gmail.com>
2025-12-02 12:20:14 -0400

    19no Commit del proyecto

--- app.py
+++ app.py
@@ -13,23 +13,14 @@ app.secret_key = "clave_super_secreta"  # cámbiala por una segura en producció
 # ----------------------
 
 def conectar_bd():
-    return psycopg2.connect(
-        host="db.prmhxjrvypxqiyqizvyx.supabase.co",
-        database="postgres",
-        user="postgres",          # el rol que creaste con permisos CRUD
-        password="EcH22025",
-        port=5432,
-        options='-c client_encoding=UTF8'
-    )
+     DATABASE_URL = os.getenv("DATABASE_URL")
+     conn = psycopg2.connect(DATABASE_URL)
+     return conn
+    
 def conectar_bd_lector():
-    return psycopg2.connect(
-        host="db.prmhxjrvypxqiyqizvyx.supabase.co",
-        database="postgres",
-        user="postgres",          # rol solo lectura
-        password="EcH22025",
-        port=5432,
-        options='-c client_encoding=UTF8'
-    )
+     DATABASE_URL = os.getenv("DATABASE_URL")
+     conn = psycopg2.connect(DATABASE_URL)
+     return conn
 
 # ----------------------
 # Decorador login (único y consistente)
